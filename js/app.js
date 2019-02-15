d7f0ee48ebf39ca6331834af181aa3b281724590
MICHAL PETRYCKI (1707586) <1707586@rgu.ac.uk>
2019-02-15 15:48:27 +0000

    Added images to icons. Set initial positions for icons.

--- js/app.js
+++ js/app.js
@@ -22,17 +22,17 @@ app.controller('mainController', ['$scope', '$uibModal', '$http', function ($sco
 
     vm.items = [
 
-      { id: '0', name: 'icon1', checked: false, description: 'Mail' },
-      { id: '1', name: 'icon2', checked: false, description: 'Calendar' },
-      { id: '2', name: 'icon3', checked: false, description: 'Photo Album' },
-      { id: '3', name: 'icon4', checked: false, description: 'Weather' },
-      { id: '4', name: 'icon5', checked: false, description: 'Facebook' },
-      { id: '5', name: 'icon6', checked: false, description: 'Instagram' },
-      { id: '6', name: 'icon7', checked: false, description: 'Music' },
-      { id: '7', name: 'icon8', checked: false, description: 'News' },
-      { id: '8', name: 'icon9', checked: false, description: 'Notes' },
-      { id: '9', name: 'icon10', checked: false, description: 'Money' },
-      { id: '10', name: 'icon11', checked: false, description: 'Horoscope' }
+      { id: '0', name: 'icon1', checked: false, description: 'Instagram' },
+      { id: '1', name: 'icon2', checked: false, description: 'Twitter' },
+      { id: '2', name: 'icon3', checked: false, description: 'Calendar' },
+      { id: '3', name: 'icon4', checked: false, description: 'Cookbook' },
+      { id: '4', name: 'icon5', checked: false, description: 'Photo Album' },
+      { id: '5', name: 'icon6', checked: false, description: 'Pig' },
+      { id: '6', name: 'icon7', checked: false, description: 'Sport' },
+      { id: '7', name: 'icon8', checked: false, description: 'Radio' },
+      { id: '8', name: 'icon9', checked: false, description: 'Horoscope' },
+      { id: '9', name: 'icon10', checked: false, description: 'Facebook' },
+      // { id: '10', name: 'icon11', checked: false, description: 'Horoscope' }
 
     ];
 
@@ -60,9 +60,6 @@ app.controller('mainController', ['$scope', '$uibModal', '$http', function ($sco
         vm.categoriesCount++;
       else
         vm.categoriesCount--;
-
-      if(vm.categoriesCount > 5)
-        console.log('Only 5 categories.');
 
     };
 
