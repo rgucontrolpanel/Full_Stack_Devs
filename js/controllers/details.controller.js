6ece2b14af5809d871425fa9fbe1ded41b45bb25
MICHAL PETRYCKI (1707586) <1707586@rgu.ac.uk>
2019-03-01 15:33:08 +0000

    Stock Market API added.

--- js/controllers/details.controller.js
+++ js/controllers/details.controller.js
@@ -97,6 +97,55 @@ angular.module('mainApp').controller('detailsController', ['$uibModalInstance', 
 
     };
 
+  }else if(item === 'Pig'){
+
+    detCtrl.stockItems = [];
+
+    var stockApi = {
+      method: 'get',
+      url: 'https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/beta/ref-data/symbols?token=sk_c131aad5c80d4c77bcdfb71dea77cf3a',
+      headers:{
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+
+    $http(stockApi).then(function(response){
+
+      // console.log(response);
+      // angular.forEach(response.data, function(value, key){
+      //
+      //   var s = { id: key, symbol: value.symbol, name: value.name };
+      //   detCtrl.stockItems.push(s);
+      //
+      // });
+
+      for(var i = 0; i < 100; i++){
+
+        var s = { id: i, symbol: response.data[i].symbol, name: response.data[i].name };
+        detCtrl.stockItems.push(s);
+
+      }
+
+    });
+
+    detCtrl.getCompanyDetails = function (symbol){
+      
+      console.log(symbol);
+      var stockApi = {
+        method: 'get',
+        url: 'https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/beta/stock/' + symbol + '/company/?token=sk_c131aad5c80d4c77bcdfb71dea77cf3a',
+        headers:{
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
+
+      $http(stockApi).then(function(response){
+
+        console.log(response);
+
+      });
+    }
+
   }
 
   detCtrl.ok = function () {
