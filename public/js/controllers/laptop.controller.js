c615b726fa73c1d149fec0afc086c66ca937e295
m_petrycki <mpetrycki@mtecsoftware.com>
2019-03-08 11:24:32 +0000

    Added context menu. Small calendar/stock market changes.

--- public/js/controllers/laptop.controller.js
+++ public/js/controllers/laptop.controller.js
@@ -3,6 +3,25 @@ angular.module('mainApp').controller('laptopController', ['$uibModalInstance', '
   var vm = this;
   vm.item = item;
 
+  vm.mood = '';
+
+  vm.contacts = [
+    { name: 'Alessandro Del Piero', value: 'alessandro' },
+    { name: 'Iga Ninja', value: 'iga' },
+    { name: 'Justeen Grindewald', value: 'justeen' },
+    { name: 'Michelangelo', value: 'michel' },
+    { name: 'Thomas Aquinas', value: 'aquinas' },
+    { name: 'Enzo Ferrari', value: 'ferrari' }
+  ];
+
+  vm.moods = [
+    { id: '0', mood: 'Happy'},
+    { id: '1', mood: 'Busy'},
+    { id: '2', mood: 'Excited'},
+    { id: '3', mood: 'Sad'},
+    { id: '4', mood: 'Sleepy'}
+  ];
+
   vm.ok = function () {
       $uibModalInstance.close();
   };
